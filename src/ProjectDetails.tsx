4d0f93913dd5ddcc4ba577075ebf506d949e91e2
fish895623 <dan990429@gmail.com>
2025-03-19 00:50:57 +0900

    feat: Fetch and display todos in ProjectDetails component with an "Add Todo" button

--- src/ProjectDetails.tsx
+++ src/ProjectDetails.tsx
@@ -1,12 +1,32 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Todo } from "./App";
 
 const ProjectDetails = () => {
   const { id } = useParams();
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    fetch(`/api/todos`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setTodos(data));
+  }, [id]);
 
   return (
     <div>
       <h1>Project Details</h1>
       <p>Project ID: {id}</p>
+      <button onClick={() => console.log("Add Todo Clicked")}>Add Todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
