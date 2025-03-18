550423540c64fc40a9fb539a572d28b9306f67b4
fish895623 <dan990429@gmail.com>
2025-03-18 10:21:35 +0900

    Project Name

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 function App() {
+  const [projectName, setProjectName] = useState("");
+
   return (
     <>
-        <h1>Projects</h1>
+      <h1>Projects</h1>
+      <div>
+        <label htmlFor="projectName">Project Name:</label>
+        <input
+          type="text"
+          id="projectName"
+          name="projectName"
+          value={projectName}
+          onChange={(e) => setProjectName(e.target.value)}
+        />
+        <button onClick={() => console.log(projectName)}>Create Project</button>
+      </div>
     </>
   );
 }
